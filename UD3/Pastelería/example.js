4c823083247ee3660ecf4ecc0ce5248fa2ad4ffd
romitus <romitus>
2020-12-10 17:31:54 +0100

    Tarea Pasteleria

---
+++ UD3/Pastelería/example.js
@@ -0,0 +1,14 @@
+$('#popoverLearn').popover({ trigger: "hover" });
+$('#popoverimage').popover({ trigger: "hover" });
+$('#popoverimage1').popover({ trigger: "hover" });
+$('#popoverimage2').popover({ trigger: "hover" });
+$('#popoverimage3').popover({ trigger: "hover" });
+$('#popoverimage4').popover({ trigger: "hover" });
+$('#popoverimage5').popover({ trigger: "hover" });
+$('#popoverimage6').popover({ trigger: "hover" });
+
+
+$('darkMode').click(function() {
+  $('.nav-link').removeClass('text-body').addClass('text-light');
+  $(this).addClass('text-light').removeClass('text-body');
+});
